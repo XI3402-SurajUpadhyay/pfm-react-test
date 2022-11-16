7087952c0d5375d6889b8eed66ee7d328e704f9b
Suraj Upadhyay <suraj.upadhyay@XEBIAINDIA.LOCAL>
2022-11-16 11:29:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Sunburst } from "pfm-components-library";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [userId, setUserId] = useState(3019);
+
+  const loadChart = () => {
+
+    if (!userId || typeof Number(userId) !== "number") {
+      alert("invalid user id");
+      return;
+    }
+
+    Sunburst({
+      endpoint: "https://api.xpsapps.xebia.com/api/v2/transaction/searchSA",
+      payload: {
+        filterList: [
+          {
+            filterType: "CustIdFilter",
+
+            filterValues: [userId],
+          },
+        ],
+      },
+      width: 600,
+      targetId: "myChart",
+    });
+  };
+
+  useEffect(() => {
+    loadChart();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1> Spend Analyser  </h1>
+      <div>
+
+      <br /> <br />
+        <input value={userId} onChange={(e) => setUserId(+e.target.value)} />
+        <button type="button" onClick={loadChart}>
+          Render Spend Analyser
+        </button>
+
+        <br /> <br />
+        <hr/>
+      </div>
+     
+     
+     <div id="myChart"></div>
+    
     </div>
   );
 }
